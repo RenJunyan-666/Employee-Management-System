b410444b982a550c5718b4037d5d22ccec47b31c
Junyan <junyan@etha.one>
2022-11-01 17:23:49 -0400

    update employee list and header footer in react

---
+++ frontend/src/components/EmployeeList.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import EmployeeService from "../services/EmployeeService";
+
+export default function EmployeeList() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    EmployeeService.getEmployees().then((res) => {
+      setEmployees(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">Employee List</h2>
+      <div className="row">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Employee First Name</th>
+              <th>Employee Last Name</th>
+              <th>Employee Email Id</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.id}>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.emailId}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a href="https://javaguides.net" className="navbar-brand">
+              Employee Management App
+            </a>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
